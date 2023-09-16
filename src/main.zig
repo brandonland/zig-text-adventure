6fb54d958aadb89896c176e5f2e67e42e56f2d35
brandonland <brandon@brandon.land>
2023-09-16 06:25:37 -0500

    re-add the hashmap, still need to fix the ugliness at the bottom :O

--- src/main.zig
+++ src/main.zig
@@ -42,65 +42,76 @@ fn isItem() bool {
 }
 
 const Cmd = struct {
-    fn help() void {
+    fn help(other_words: ?[]const u8) void {
         print("Help menu\n", .{});
+        _ = other_words;
     }
 
-    fn go(direction: []const u8) void {
-        print("You go {s}\n", .{direction});
+    fn go(other_words: ?[]const u8) void {
+        print("You go somewhere.\n", .{});
+        _ = other_words;
     }
 
-    fn examine(object: []const u8) void {
-        print("You examine {s}\n", .{object});
+    fn examine(other_words: ?[]const u8) void {
+        print("You examine the object\n", .{});
+        _ = other_words;
     }
 
-    fn look() void {
+    fn look(other_words: ?[]const u8) void {
         // This function depends on the location of the player.
         print("You look around.\n", .{});
+        _ = other_words;
     }
 
-    fn take(item: []const u8) void {
+    fn take(other_words: ?[]const u8) void {
         // Need logic here to determine if the object can be taken.
         // Is the object actually an item?
         //if (!item.isItem()) {
         //    print("This object cannot be taken.", .{});
         //}
-        print("You take {s}\n", .{item});
+        print("You take the object\n", .{});
+        _ = other_words;
     }
 
-    fn drop() void {
+    fn drop(other_words: ?[]const u8) void {
         print("You drop it like it's hot\n", .{});
+        _ = other_words;
     }
 
-    fn open() void {
+    fn open(other_words: ?[]const u8) void {
         print("You open it.\n", .{});
+        _ = other_words;
     }
 
-    fn put(object: anytype) void {
+    fn put(other_words: ?[]const u8) void {
         // TODO: Implement "put it in" vs "put it on"
-        _ = object;
-
         print("You put it somewhere.\n", .{});
+        _ = other_words;
     }
 
-    fn push() void {
+    fn push(other_words: ?[]const u8) void {
         print("You push it.\n", .{});
+        _ = other_words;
     }
 
-    fn pull() void {
+    fn pull(other_words: ?[]const u8) void {
         print("You pull it.\n", .{});
+        _ = other_words;
     }
 
-    fn turn() void {
+    fn turn(other_words: ?[]const u8) void {
         print("You turn it.\n", .{});
+        _ = other_words;
     }
 
-    fn feel() void {
+    fn feel(other_words: ?[]const u8) void {
         print("You feel it.\n", .{});
+        _ = other_words;
     }
 
-    fn eat() void {
+    fn eat(other_words: ?[]const u8) void {
         print("You eat it.\n", .{});
+        _ = other_words;
     }
 
     fn unknown() void {
@@ -108,11 +119,20 @@ const Cmd = struct {
     }
 };
 
-// Commands:
+// All commands:
 const commands = [_][]const u8{
     "help", "h",    "go",   "examine", "x",   "look", "l", "take", "drop", "open", "put",
     "push", "pull", "turn", "feel",    "eat",
 };
+
+// These commands can be called with just 1 word
+const one_word_commands = [_][]const u8{
+    "help", "h", "look", "l", "feel",
+};
+
+// These commands are called with 2 or more words.
+// Note: some of these can also be one-word commands.
+const two_word_commands = [_][]const u8{ "go", "examine", "x", "" };
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
@@ -128,6 +148,24 @@ pub fn main() !void {
         .wielding = null,
         .gear = null,
     };
+
+    var cmds = std.StringHashMap(*const fn (?[]const u8) void).init(std.heap.page_allocator);
+    try cmds.put("help", Cmd.help);
+    try cmds.put("h", Cmd.help);
+    try cmds.put("examine", Cmd.examine);
+    try cmds.put("x", Cmd.examine);
+    try cmds.put("look", Cmd.look);
+    try cmds.put("l", Cmd.look);
+    try cmds.put("take", Cmd.take);
+    try cmds.put("drop", Cmd.drop);
+    try cmds.put("open", Cmd.open);
+    try cmds.put("put", Cmd.put);
+    try cmds.put("push", Cmd.push);
+    try cmds.put("pull", Cmd.pull);
+    try cmds.put("turn", Cmd.turn);
+    try cmds.put("feel", Cmd.feel);
+    try cmds.put("eat", Cmd.eat);
+    try cmds.put("go", Cmd.go);
 
     // Print the current room's description
     try stdout.print("{s}\n", .{map.rooms[p_state.room].description});
@@ -154,38 +192,68 @@ pub fn main() !void {
             return;
         };
 
-        var it = std.mem.split(u8, input_line, " ");
+        var it = std.mem.splitAny(u8, input_line, " ");
         const first_word = it.next();
         const second_word = it.next();
 
+        it.reset();
+
+        var it_len: usize = 0;
+        while (it.next() != null) {
+            it_len += 1;
+        }
+
         var match = false;
 
-        for (commands) |command| {
-            if (eql(u8, first_word.?, command)) {
-                match = true;
-
-                if (eql(u8, first_word.?, "help")) Cmd.help();
-                if (eql(u8, first_word.?, "h")) Cmd.help();
-                if (eql(u8, first_word.?, "examine")) {
-                    //print("Amount of words: {}", .{it.});
-                    Cmd.examine(second_word.?);
+        // TODO: Nested if statements are ugly. This logic needs refactored and restructured.
+        // Need to move logic to the command functions.
+        //
+        // Separate commands that require only 1 word and commands that require more.
+        // Some commands fall under both categories (can have multiple or just 1 word)
+        if (it_len == 1) {
+            for (one_word_commands) |command| {
+                if (eql(u8, first_word.?, command)) {
+                    match = true;
+                    if (eql(u8, first_word.?, "help")) Cmd.help(it.rest());
+                    if (eql(u8, first_word.?, "h")) Cmd.help(it.rest());
+                    if (eql(u8, first_word.?, "look")) Cmd.look(it.rest());
+                    if (eql(u8, first_word.?, "l")) Cmd.look(it.rest());
                 }
-                if (eql(u8, first_word.?, "x")) Cmd.examine(second_word.?);
-                if (eql(u8, first_word.?, "look")) Cmd.look();
-                if (eql(u8, first_word.?, "l")) Cmd.look();
-                if (eql(u8, first_word.?, "take")) Cmd.take(second_word.?);
-                if (eql(u8, first_word.?, "drop")) Cmd.drop();
-                if (eql(u8, first_word.?, "open")) Cmd.open();
-                if (eql(u8, first_word.?, "put")) Cmd.put(second_word.?);
-                if (eql(u8, first_word.?, "push")) Cmd.push();
-                if (eql(u8, first_word.?, "pull")) Cmd.pull();
-                if (eql(u8, first_word.?, "turn")) Cmd.turn();
-                if (eql(u8, first_word.?, "feel")) Cmd.feel();
-                if (eql(u8, first_word.?, "eat")) Cmd.eat();
-                if (eql(u8, first_word.?, "go")) Cmd.go(second_word.?);
-                break;
+            }
+        } else if (it_len == 2) {
+            for (commands) |command| {
+                if (eql(u8, first_word.?, command)) {
+                    match = true;
+
+                    // TODO: this is hardcoded
+                    if (eql(u8, first_word.?, "examine")) {
+                        if (it_len != 2) {
+                            print("This command requires exactly two words. Try again.\n", .{});
+                            print("(e.g: {s} door)\n", .{first_word.?});
+                            break;
+                        }
+                        Cmd.examine(second_word.?);
+                    }
+                    if (eql(u8, first_word.?, "help")) Cmd.help(it.rest());
+                    if (eql(u8, first_word.?, "h")) Cmd.help(it.rest());
+                    if (eql(u8, first_word.?, "look")) Cmd.look(it.rest());
+                    if (eql(u8, first_word.?, "l")) Cmd.look(it.rest());
+                    if (eql(u8, first_word.?, "x")) Cmd.examine(it.rest());
+                    if (eql(u8, first_word.?, "take")) Cmd.take(it.rest());
+                    if (eql(u8, first_word.?, "drop")) Cmd.drop(it.rest());
+                    if (eql(u8, first_word.?, "open")) Cmd.open(it.rest());
+                    if (eql(u8, first_word.?, "put")) Cmd.put(it.rest());
+                    if (eql(u8, first_word.?, "push")) Cmd.push(it.rest());
+                    if (eql(u8, first_word.?, "pull")) Cmd.pull(it.rest());
+                    if (eql(u8, first_word.?, "turn")) Cmd.turn(it.rest());
+                    if (eql(u8, first_word.?, "feel")) Cmd.feel(it.rest());
+                    if (eql(u8, first_word.?, "eat")) Cmd.eat(it.rest());
+                    if (eql(u8, first_word.?, "go")) Cmd.go(it.rest());
+                    break;
+                }
             }
         }
-        if (!match) Cmd.unknown();
+
+        if (!match) Cmd.unknown(it.rest());
     }
 }
